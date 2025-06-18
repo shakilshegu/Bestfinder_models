087eaab16c2dd3234c9575a3af685d5c0c14be6e
SHAKIL <124359123+shakilshegu@users.noreply.github.com>
2025-06-18 15:44:22 +0530

    Update Amenity.js

--- Amenity.js
+++ Amenity.js
@@ -1,1 +1,24 @@
 
+import mongoose from 'mongoose';
+
+const amenitySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    icon: {
+        type: String,
+        trim: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true
+});
+
+const Amenity = mongoose.model('Amenity', amenitySchema);
+export default Amenity;
